250cac31dbf78855c6ad3131987c0dbb62bbd749
Ekaterina <ekaterina.shnaygina02@mail.ru>
2023-03-21 22:26:20 +0300

    первая фиксация

---
+++ HomeWork_1/2.py
@@ -0,0 +1,3 @@
+# 2 задача:
+n = str(input ())
+print(int(n[0]) +int(n[1]) +int(n[2]))
---
+++ HomeWork_1/6.py
@@ -0,0 +1,6 @@
+# 6 задача
+n = str(input ()) 
+if int(n[0]) +int(n[1]) +int(n[2]) ==int(n[3]) + int(n[4]) + int(n[5]):
+    print("yes") 
+else:
+    print("no")
